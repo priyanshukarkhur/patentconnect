57fe0d344eb923ad50590350cb3abe9de5992bf1
priyanshukarkhur <priyanshu.karkhur99@gmail.com>
2025-11-09 17:17:40 +0530

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,15 +1,16 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+// lib/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBvrA0VEB-t-CIQNImXCG_avZGrDRPYhPQ",
   authDomain: "patenthive-54b04.firebaseapp.com",
   projectId: "patenthive-54b04",
-  storageBucket: "patenthive-54b04.appspot.com", // ✅ Correct bucket
+  storageBucket: "patenthive-54b04.appspot.com",
   messagingSenderId: "155966109917",
   appId: "1:155966109917:web:0df47390d7457d4aa0c334"
 };
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export default app;
+export const provider = new GoogleAuthProvider();
